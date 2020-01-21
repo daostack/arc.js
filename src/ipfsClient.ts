d89a189908d93c180ce0ce1db0642e5a3caf631b
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2020-01-21 20:25:21 +0100

    add ipfs.cat

--- src/ipfsClient.ts
+++ src/ipfsClient.ts
@@ -13,6 +13,21 @@ export class IPFSClient {
     return this.baseUrl
   }
 
+  public async cat(hash: string) {
+    const params = new FormData()
+    params.append('arg', Buffer.from(hash))
+    const response = await fetch(`${this.baseUrl}/cat`, {
+        body: params,
+        method: 'POST'
+    })
+    if (response.status !== 200) {
+        const msg = await response.text()
+        throw Error(`An error occurred getting the file ${hash}: ${msg}`)
+    }
+    const result = await response.json()
+    return result
+
+  }
   public async addString(data: string) {
     const params = new FormData()
     const buffer = Buffer.from(data)
