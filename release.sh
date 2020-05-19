e25c10f490f2cf6e5c111e43944aa707a0fa1dbf
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2020-05-19 17:12:57 +0200

    Package version 2.0.0-experimental.33

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e # fail on error
 echo "Creating a new release"
-npm ci
+# npm ci
 package_version=$(cat package.json | jq -r '.version')
 
 # tag on github
 echo "create tag ${package_version}"
-git ci -a -m "Package version ${package_version}"
+git ci -a -m "Package version ${package_version}" --allow-empty
 git tag -a $package_version -m "Release of version $package_version"
 git push --tags
 
