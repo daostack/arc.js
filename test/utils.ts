d8f6c497327ba97a4ea05e96bf4ecc06bd23aa83
jelle <jellegerbrandy@gmail.com>
2020-05-26 15:10:35 +0200

    upgrade to rc.21

--- test/utils.ts
+++ test/utils.ts
@@ -69,7 +69,7 @@ export interface ITestAddresses {
 export function getTestAddresses(version: string = LATEST_ARC_VERSION): ITestAddresses {
   const result = require('@daostack/test-env-experimental/daos.json').demo[version]
   if (!result) {
-    throw Error(`No test addresses found; please check the version of @dorgtech/test-env-experimental and LATEST_ARC_VERSION`)
+    throw Error(`No test addresses found; please check the version of @daostack/test-env-experimental and LATEST_ARC_VERSION`)
   }
   return result
 }
