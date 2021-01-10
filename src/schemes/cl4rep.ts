68993c52b63f3480f2d1e5f8de09319b69173224
Oren Sokolowsky <oren@daostack.io>
2021-01-10 10:36:19 +0200

    use Decimal.js

--- src/schemes/cl4rep.ts
+++ src/schemes/cl4rep.ts
@@ -1,4 +1,5 @@
 import BN = require('bn.js')
+import {Decimal} from 'decimal.js'
 import { from } from 'rxjs'
 import { concatMap } from 'rxjs/operators'
 import { realMathToBN, realMathToNumber } from '../utils'
@@ -12,7 +13,7 @@ import { Address } from '../types'
 
 import { Scheme } from '../scheme'
 
-export class CTL4RScheme {
+export class CL4RScheme {
 
   constructor(public scheme: Scheme) {
 
@@ -30,13 +31,11 @@ export class CTL4RScheme {
   public async getRepuationRewardForBatch(repRewardConstA: string,
                                           repRewardConstB: string,
                                           batchIndex: number)
-                                          : Promise<BN> {
-    // repRewardConstB is alway a realNumber value < 1 and > 0 so lets convert it to numnber between 0 to 1000
-    const constBMull1000 = new BN(Math.round((realMathToNumber(new BN(repRewardConstB)) + Number.EPSILON) * 100) * 10)
-    // (repRewardConstA *  (repRewardConstB ** batchIndex) / (1000 ** batchIndex)
-    return realMathToBN(new BN(repRewardConstA))
-           .mul(constBMull1000.pow(new BN(batchIndex)))
-           .div((new BN(1000)).pow(new BN(batchIndex)))
+                                          : Promise<Decimal> {
+    const constB = new Decimal(realMathToNumber(new BN(repRewardConstB)))
+    const constA = new Decimal(realMathToBN(new BN(repRewardConstA)).toString())
+    return constA.times(constB.toPower(batchIndex))
+
   }
 
   public async getAgreementHash(): Promise<string> {
@@ -47,17 +46,17 @@ export class CTL4RScheme {
 
   public async getReputationRewardForLockingIds(lockingIds: number[],
                                                 batchIndex: number,
-                                                repuationRewardForPeriod: BN)
-                                                : Promise<BN> {
-    let reputation = new BN('0')
+                                                repuationRewardForPeriod: Decimal)
+                                                : Promise<Decimal> {
+    let reputation = new Decimal('0')
     const contract = await this.getContract()
-    const lockingTotalScore = new BN(await contract.methods.batches(batchIndex).call())
+    const lockingTotalScore = new Decimal(await contract.methods.batches(batchIndex).call())
 
     if (lockingTotalScore.isZero()) {
       return lockingTotalScore
     }
     for (const lockingId of lockingIds) {
-      const lockingIdScore = new BN(await contract.methods.getLockingIdScore(batchIndex, lockingId).call())
+      const lockingIdScore = new Decimal(await contract.methods.getLockingIdScore(batchIndex, lockingId).call())
       reputation = reputation.add(lockingIdScore.div(lockingTotalScore).mul(repuationRewardForPeriod))
     }
     return reputation
